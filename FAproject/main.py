28f3b0098c6cd037e447912f19830d38420c3bde
Владислав Гмырь <gmyr@sputnikfund.ru>
2022-09-19 18:12:26 +0700

    Refactor: delete unused libraries, add middleware

--- FAproject/main.py
+++ FAproject/main.py
@@ -1,8 +1,15 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from endpoints import random_values
+import time
 
-app = FastAPI(title="HW FastAPI")
+app = FastAPI(
+    title="HW FastAPI",
+    description="App for doing corse's HW",
+    version="0.0.1",
+    docs_url="/docs",
+    redoc_url="/docs/redoc",
+)
 
 app.include_router(random_values.router, prefix="/random", tags=["Random values generator"])
 
@@ -20,6 +27,15 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     print('App has finished!')
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):  # noqa: D103
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
 
 
 if __name__ == '__main__':
--- FAproject/models/random_values.py
+++ FAproject/models/random_values.py
@@ -1,9 +1,7 @@
 from pydantic import BaseModel
-from typing import Optional
 
 
 class RandInt(BaseModel):
     value: int
     min_value: int
     max_value: int
-
